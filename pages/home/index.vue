28e61606a46e6f9d3068802fc3e4b9d5496710e5
cc <454355131@qq.com>
2022-11-11 15:51:07 +0800

    create

---
+++ pages/home/index.vue
@@ -0,0 +1,89 @@
+<template>
+	<div class="flexColAllWidthCls">
+		<div style="margin: 10px 0;width: 90%;">
+			<uni-easyinput type="search" suffixIcon="search"  v-model="searchGoodsName" placeholder="搜索更多折扣" @iconClick="onClick"></uni-easyinput>
+		</div>
+		<div style="margin-top: 10px;border-radius: 10px;width: 90%;background-color: #fff;">
+			<div class="flexColCls" style="margin: 10px;">
+				<div style="color: #3D3D3D;font-size: 14px;font-weight: 700;width: 100%;text-align: left;">
+					今日活动专区
+				</div>
+			</div>
+		</div>
+		<uni-popup ref="getOpenIdErrPop" type="message">
+			<uni-popup-message type="success" message="获取openId成功" :duration="2000"></uni-popup-message>
+		</uni-popup>
+	</div>
+</template>
+
+<script>
+	import { mapActions } from 'vuex'
+	export default{
+		name: "HomeIndex",
+		data(){
+			return {
+				currentOpenId: this.$store.state.openId,
+				searchGoodsName: "",
+				activityAreaList: [], //活动专区列表
+			}
+		},
+		onLoad() {
+			console.log("dddddddddddd");
+			console.log(this.currentOpenId);
+			if(!this.currentOpenId){
+				this.getOpenId();
+			}
+		},
+		created() {
+			
+		},
+		methods: {
+			...mapActions(['getUserOpenId']),
+			change(){
+				
+			},
+			async getOpenId(){
+				var res = await this.getUserOpenId();
+				console.log('rrrrrrrresss', res);
+				if(res){
+					this.currentOpenId = res;
+					this.$refs.getOpenIdErrPop.open('top');
+					console.log('fffffffffffffffffffff', uni.getUserProfile);
+					uni.showModal({
+						title:'授权',
+						content:"是否授权",
+						success: () => {
+							uni.getUserProfile({
+								desc: "获取你的昵称、头像信息",//必填项，声明获取用户个人信息后的用途，不超过30个字符
+								success: (res) => {
+								  const userInfo = res.userInfo;
+								  console.log("用户基本信息", userInfo);
+								},
+								fail: (res) => {
+									console.log(res);
+								  //拒绝授权
+								  wx.showToast({
+									title: "获取失败",
+									icon: "error",
+									duration: 2000,
+									});
+								  return;
+								},
+							});
+						}
+					})
+				}
+				// console.log('666666666666666666666666',this.$store.state.openId);
+				// if(this.currentOpenId){
+				// 	this.$refs.getOpenIdErrPop.open('top');
+				// }
+			}
+		}
+	}
+</script>
+
+<style>
+	.searchCls{
+		border-radius: 10px;
+	}
+</style>
---
+++ store/index.js
@@ -0,0 +1,147 @@
+// #ifndef VUE3
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store({
+// #endif
+
+// #ifdef VUE3
+import { createStore } from 'vuex'
+const store = createStore({
+// #endif
+	state: {
+		hasLogin: false,
+		isUniverifyLogin: false,
+		loginProvider: "",
+		openId: null,
+		testvuex: false,
+		colorIndex: 0,
+		colorList: ['#FF0000', '#00FF00', '#0000FF'],
+		noMatchLeftWindow: true,
+		active: 'componentPage',
+		leftWinActive: '/pages/component/view/view',
+		activeOpen: '',
+		menu: [],
+		univerifyErrorMsg: ''
+	},
+	mutations: {
+		login(state, provider) {
+			state.hasLogin = true;
+			state.loginProvider = provider;
+		},
+		logout(state) {
+			state.hasLogin = false
+			state.openId = null
+		},
+		setopenId(state, openId) {
+			state.openId = openId
+			console.log('sssss', state.openId);
+		},
+		setTestTrue(state) {
+			state.testvuex = true
+		},
+		setTestFalse(state) {
+			state.testvuex = false
+		},
+		setColorIndex(state, index) {
+			state.colorIndex = index
+		},
+		setMatchLeftWindow(state, matchLeftWindow) {
+			state.noMatchLeftWindow = !matchLeftWindow
+		},
+		setActive(state, tabPage) {
+			state.active = tabPage
+		},
+		setLeftWinActive(state, leftWinActive) {
+			state.leftWinActive = leftWinActive
+		},
+		setActiveOpen(state, activeOpen) {
+			state.activeOpen = activeOpen
+		},
+		setMenu(state, menu) {
+			state.menu = menu
+		},
+		setUniverifyLogin(state, payload) {
+			typeof payload !== 'boolean' ? payload = !!payload : '';
+			state.isUniverifyLogin = payload;
+		},
+		setUniverifyErrorMsg(state,payload = ''){
+			state.univerifyErrorMsg = payload
+		}
+	},
+	getters: {
+		currentColor(state) {
+			return state.colorList[state.colorIndex]
+		}
+	},
+	actions: {
+		// lazy loading openId
+		getUserOpenId: async function({
+			commit,
+			state
+		}) {
+			return await new Promise((resolve, reject) => {
+				if (state.openId) {
+					resolve(state.openId)
+				} else {
+					uni.login({
+						provider: "weixin",
+						success: (data) => {
+							commit('login')
+							let appid = "wx0724a71a0913f7ae";
+							let secret = "33213cffc09e57eaf7d946f8c30d7788";
+							let url =
+								"https://api.weixin.qq.com/sns/jscode2session?appid=" +
+								appid +
+								"&secret=" +
+								secret +
+								"&js_code=" +
+								data.code +
+								"&grant_type=authorization_code";
+							uni.request({
+								url: url, // 请求路径
+								success: (r) => {
+								  console.log("r", r);
+								  console.info("用户的openId", r.data.openid);
+								  commit('setopenId', r.data.openid)
+								  resolve(r.data.openid)
+								},
+							  });
+							// commit('setopenId', openId)
+							
+						},
+						fail: (err) => {
+							console.log('uni.login 接口调用失败，将无法正常使用开放接口等服务', err)
+							reject(err)
+						}
+					})
+				}
+			})
+		},
+		getPhoneNumber: function({
+			commit
+		}, univerifyInfo) {
+			return new Promise((resolve, reject) => {
+				uni.request({
+					url: 'https://97fca9f2-41f6-449f-a35e-3f135d4c3875.bspapp.com/http/univerify-login',
+					method: 'POST',
+					data: univerifyInfo,
+					success: (res) => {
+						const data = res.data
+						if (data.success) {
+							resolve(data.phoneNumber)
+						} else {
+							reject(res)
+						}
+
+					},
+					fail: (err) => {
+						reject(res)
+					}
+				})
+			})
+		}
+	}
+})
+
+export default store
